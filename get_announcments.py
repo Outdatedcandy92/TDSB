739002869a46a1141ddc2d844f0f47e3e2e7bee8
Outdatedcandy92 <138517406+Outdatedcandy92@users.noreply.github.com>
2024-08-24 23:33:44 -0400

    first commit

---
+++ get_announcments.py
@@ -0,0 +1,30 @@
+import requests
+import os
+import requests
+from dotenv import load_dotenv
+import json
+
+load_dotenv()
+
+school = os.getenv("SCHOOL_CODE")
+
+url = f"https://zappsmaprd.tdsb.on.ca/api/Announcement/D2L/GetNews/1013/filter/Published/skip/0/take/10"
+
+headers = {
+    "X-Client-App-Info": "Android||2024Oct01120000P|False|1.2.6|False|306|False",
+    "Authorization": "Bearer " + os.getenv("TOKEN")
+}
+
+response = requests.get(url, headers=headers)
+
+if response.status_code == 200:
+    data = response.json()
+    print("Request successful!")
+    print("Response:", data)
+    with open("logs/get_announment.json", "w") as file:
+        json.dump(data, file, indent=4)
+    print("JSON data stored in get_announment.json")
+else:
+    print("Request failed with status code:", response.status_code)
+    print("Response Text:", response.text)
+    print("Response Headers:", response.headers)
